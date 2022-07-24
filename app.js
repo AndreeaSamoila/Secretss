2a8ade928381a8f08b04f18c7aca5459d5f574eb
samoila <asdfghjk>
2022-07-24 22:37:50 +0300

    oAuth2.0

--- app.js
+++ app.js
@@ -3,17 +3,20 @@
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs")
+const ejs = require("ejs");
 const mongoose = require("mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-find-or-create");
 
 // const encrypt = require("mongoose-encryption")
 // const md5 = require('md5');
 // const bcrypt = require("bcrypt");
 // const saltRounds = 10;
 
-const session = require('express-session')
-const passport = require('passport')
+const session = require('express-session');
+const passport = require('passport');
 const passportLocalMongoose = require("passport-local-mongoose");
+
 
 const app = express();
 console.log(process.env.API_KEY);
@@ -23,9 +26,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
-
 app.use(session({
-    secret: 'Our little secret.', resave: false, saveUninitialized: true,
+    secret: 'Our little secret.', resave: false, saveUninitialized: false
 }));
 
 app.use(passport.initialize());
@@ -35,11 +37,14 @@ mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
 
 const userSchema = new mongoose.Schema({
-    email: String, password: String
+    email: String,
+    password: String,
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
-
+userSchema.plugin(findOrCreate);
 // const secret = "Thisisourlittlesecret";
 
 // userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
@@ -47,12 +52,49 @@ userSchema.plugin(passportLocalMongoose);
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(null, user);
+    })
+})
+
+passport.use(new GoogleStrategy({
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/secrets",
+        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+    },
+    function (accessToken, refreshToken, profile, cb) {
+        console.log('profile: ', profile);
+        User.findOrCreate({googleId: profile.id}, function (err, user) {
+            console.log('err', err);
+            console.log(JSON.stringify(user, null, 4));
+            return cb(err, user);
+
+        });
+    }
+));
 
 app.get("/", function (req, res) {
     res.render("home");
 });
+
+app.get("/auth/google",
+    passport.authenticate('google', {scope: ["profile"]})
+);
+
+app.get('/auth/google/secrets',
+    passport.authenticate('google', {failureRedirect: "/login"}),
+    function (req, res) {
+        // Successful authentication, redirect home.
+        res.redirect("/secrets");
+    });
+
 
 app.get("/login", function (req, res) {
     res.render("login");
@@ -63,13 +105,7 @@ app.get("/register", function (req, res) {
 });
 
 
-app.get("/secrets", function (req, res) {
-    if (req.isAuthenticated()) {
-        res.render("secrets");
-    } else {
-        res.redirect("/login")
-    }
-});
+
 app.post("/register", function (req, res) {
 
     // bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
@@ -141,6 +177,44 @@ app.post("/login", function (req, res) {
     });
 });
 
+app.get("/secrets", function (req, res) {
+    User.find({"secret": {$ne: null}}, function(err, foundUsers){
+        if (err){
+            console.log(err);
+        } else {
+            if (foundUsers) {
+                res.render("secrets", {usersWithSecrets: foundUsers});
+            }
+        }
+    });
+});
+app.get("/submit", function (req, res) {
+
+    if (req.isAuthenticated()) {
+        res.render("submit");
+    } else {
+        res.redirect("/login")
+    }
+});
+
+app.post("/submit", function (req, res) {
+    const submittedSecret = req.body.secret;
+    console.log(req.user.id);
+    User.findById(req.user.id, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function () {
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    });
+});
+
+
 app.get("/logout", function (req, res) {
     req.logout(function (err) {
         if (err) {
@@ -156,3 +230,7 @@ app.listen(3000, function () {
 //session- a lot of time where the user interacts with a server
 
 //bcrypt or md5 or encrypte mongoose or without any
+//why OAuth
+//permite nivele de acces granular
+//read/read+write access
+//revoke access
